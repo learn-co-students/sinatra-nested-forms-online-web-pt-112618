28e21f76512e2b323df4173fd68476d544dc554f
Akili Humphrey <akilihumphrey@gmail.com>
2019-03-16 08:25:45 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name , :type , :booty
+  Ships = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+  end
+  
+   def self.all
+    Ships
+  end
+
+  def self.clear
+    Ships.clear
+  end
 end
